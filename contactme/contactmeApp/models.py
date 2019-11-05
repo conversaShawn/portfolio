f696a43202200db82c66e802fbe47b613d15fba8
Shawn Harris <shawn.harris3510@gmail.com>
2019-11-05 16:12:16 -0500

    removed pyhton

--- contactme/contactmeApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Contact(models.Model):
-    # this is like schema in express IssueItemSchema = mongoose.Schema({description : String, done : Boolean})
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    number = models.CharField(max_length=15)
-    message = models.CharField(max_length=5000)
